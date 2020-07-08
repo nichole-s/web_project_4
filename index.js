df21a38baa282d09b68ce0fe1f7ad4c091ffdaa0
nichole-s <trainer.nichole@gmail.com>
2020-07-07 22:48:37 -0500

    added functionality for forms and buttons

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
+
+// Constants related to the profile
 const editButton = document.querySelector('.profile__edit');
 const modalEdit = document.querySelector('.modal__type_edit-profile');
 const editProfileCloseButton = modalEdit.querySelector('.modal__exit_type_edit-profile');
 const editProfileSubmitButton = modalEdit.querySelector('.modal__submit_type_edit-profile');
 const formEdit = modalEdit.querySelector('.modal__form_type_edit-profile');
-const inputName = modalEdit.querySelector('.modal__name');
-const inputProfession = modalEdit.querySelector('.modal__profession');
+const inputName = formEdit.querySelector('.modal__name');
+const inputProfession = formEdit.querySelector('.modal__profession');
 
 const profileName = document.querySelector('.profile__name');
 const profileProfession = document.querySelector('.profile__profession');
 
-const addButton = document.querySelector('profile__add');
+// Constants related to adding a card
+const addButton = document.querySelector('.profile__add');
 const modalAdd = document.querySelector('.modal__type_add-card');
 const addCardCloseButton = modalAdd.querySelector('.modal__exit_type_add-card');
 const addCardSubmitButton = modalAdd.querySelector('.modal__submit_type_add-card');
 const formAdd = modalAdd.querySelector('.modal__form_type_add-card');
-const inputCardName = modalAdd.querySelector('.modal__card-name');
-const inputCardUrl = modalAdd.querySelector('.modal__card-url');
+const inputCardName = formAdd.querySelector('.modal__card-name');
+const inputCardUrl = formAdd.querySelector('.modal__card-url');
 
+
+// Constants related to the image modal
 const modalImage = document.querySelector('.modal__type_image');
 const imageCloseButton = modalImage.querySelector('.modal__exit_type_image');
+const modalImageFigure = modalImage.querySelector('.modal__image_figure');
+const modalImageCaption = modalImage.querySelector('.modal__image_caption');
 
-
+// Creating initial and new cards
 const cardTemplate = document.querySelector('.card-template').content.querySelector('.photo-grid__item');
 
 const cardList = document.querySelector('.photo-grid__items');
@@ -52,7 +59,27 @@ const initialCards = [
   }
 ];
 
-const createCard = (data) => {
+function toggleModal(modal) {
+  modal.classList.toggle('modal_visible');
+}
+
+function displayImage(title, link) {
+
+  modalImageFigure.src = link;
+  modalImageFigure.alt = title;
+  modalImageCaption.textContent = title;
+  toggleModal(modalImage);
+  
+  // const popupBackgroundImage = popupFullImage.querySelector(".popup__background");
+  // popupBackgroundImage.addEventListener("click", function() {
+  //   if (popupFullImage.classList.contains("popup_active")) {
+  //     togglePopup(popupFullImage);
+  //   }
+  // })
+};
+
+
+const createCard = (name, link) => {
   const cardElement = cardTemplate.cloneNode(true);
 
   const cardTitle = cardElement.querySelector('.photo-grid__heading');
@@ -60,11 +87,12 @@ const createCard = (data) => {
   const cardLikeButton = cardElement.querySelector('.photo-grid__like');
   const cardRemoveButton = cardElement.querySelector('.photo-grid__trash');
   
-  cardTitle.textContent = data.name;
-  cardImage.style.backgroundImage = `url(${data.link})`;
+  cardTitle.textContent = name;
+  cardImage.style.backgroundImage = `url(${link})`;
+  cardImage.style.backgroundSize = "cover";
 
-  cardLikeButton.addEventListener('click', () => {
-    cardLikeButton.classList.toggle('.photo-grid__liked');
+  cardLikeButton.addEventListener('click', (e) => {
+    e.target.closest('.photo-grid__like').classList.toggle('photo-grid__liked');
   })
 
   cardRemoveButton.addEventListener('click', (e) => {
@@ -72,87 +100,82 @@ const createCard = (data) => {
   })
 
   cardImage.addEventListener('click', (e) => {
-    e.target.closest('.modal__type_image').toggleModal(modalImage);
+    displayImage(name, link);
+
   })
 
   return cardElement;
 }
 
-const renderCard = (data) => {
-  cardList.prepend(createCard(data));
+const renderCard = (name, link) => {
+
+  cardList.prepend(createCard(name, link));
 
 }
   
 initialCards.forEach((data) => {
-  renderCard(data);
+
+  renderCard(data.name, data.link);
+
 })
 
-function toggleModal(modal) {
-  modal.classList.toggle('.modal_visible');
-}
 
 editButton.addEventListener('click', () => {
-  toggleModal(modalEdit);
+
+ toggleModal(modalEdit);
+
+ formEdit.reset();
+
 });
 
 editProfileCloseButton.addEventListener('click', () => {
+
+ toggleModal(modalEdit);
+
+});
+
+
+formEdit.addEventListener('submit', (evt) => {
+  
+  evt.preventDefault();
+
+  profileName.textContent = inputName.value;
+  profileProfession.textContent = inputProfession.value;
+  
   toggleModal(modalEdit);
+
 });
 
-editProfileSubmitButton.addEventListener('click', () => {
-  toggleModal(modalEdit);
-});
+formAdd.addEventListener('submit', (evt) => {
 
-addButton.addEventListener('click', () => {
+  evt.preventDefault();
+
+  const inputCardName = formAdd.querySelector('.modal__card-name');
+  const inputCardUrl = formAdd.querySelector('.modal__card-url');
+
+  cardList.prepend(createCard({name: inputCardName.value, link: inputCardUrl.value}));
+
   toggleModal(modalAdd);
-});
 
-addCardCloseButton.addEventListener('click', () => {
-  toggleModal(modalAdd);
-});
+});  
 
 addCardSubmitButton.addEventListener('click', () => {
   toggleModal(modalAdd);
+})
+
+addButton.addEventListener('click', () => {
+ toggleModal(modalAdd);
+ formAdd.reset();
+});
+
+addCardCloseButton.addEventListener('click', () => {
+ toggleModal(modalAdd);
+});
+
+addCardSubmitButton.addEventListener('click', () => {
+ toggleModal(modalAdd);
 });
 
 imageCloseButton.addEventListener('click', () => {
-  toggleModal(modalImage);
+ toggleModal(modalImage);
 });
-
-//function formSubmitHandler(evt) {
-//  evt.preventDefault();
-//  profileName.textContent = inputName.value
-//  profileProfession.textContent = inputProfession.value
-//  toggleModal(modalEdit);
-//}
-
-//formEdit.addEventListener('submit', formSubmitHandler);
-
-
-
-
-//formAdd.addEventListener('submit', function (e) {
-//  e.preventDefault();
-
-//  renderCard(data);
- 
-//  toggleModal(modalAdd);
-//})
-
-
-//The following is the start of transitioning to the bubbling method for event listeners. I intend on switching 
-//before submitting the final project
-
-//cardList.addEventListener('click', (e) => {
-//  if(e.target.classList.includes('.photo-grid__trash')) {  
-//    e.target.closest('.photo-grid__item').remove();
-//  }
-
-//  if(e.target.classList.includes('.photo-grid__like')) {
-//    cardLikeButton.classlist.toggle('photo-grid__liked');
-//}
-
-//  if(e.target.classList.includes('.photo-grid__photo')) {
-//    e.target.closest('.modal__type_image').toggleModal(modalImage);
-//}
-//})
